7d1d17aa32b0cd45ff3fb8a664e3e0a46b03debd
Rafael Barbosa <lul.rafaelbarbosa@gmail.com>
2024-08-05 23:04:25 -0300

    feat: add parameters in ModelV0 class

--- mlp/functions_mlp.py
+++ mlp/functions_mlp.py
@@ -20,13 +20,24 @@ def cuda_is_available():
 
 class ModelV0(nn.Module):
     
-    def __init__(self):
+    def __init__(self, n_features, neurons_hidden, n_class_pred):
         
-        super().__init__()
+        super(ModelV0, self).__init__()
         
-        self.layer_1 = nn.Linear(in_features = 20, out_features = 30, bias = True) # 20 features (dataset) to a next layer with 30 neurons
-        self.layer_2 = nn.Linear(in_features = 30, out_features = 20, bias = True) # layer with 30 neurons and define a next layer with 20 neurons
+        self.layer_1 = nn.Linear(in_features = n_features, out_features = neurons_hidden, bias = True) # 20 features (dataset) to a next layer with 30 neurons
+        self.layer_2 = nn.Linear(in_features = neurons_hidden, out_features = n_class_pred, bias = True) # layer with 30 neurons and define a next layer with 20 neurons
         
     def forward(self, x): # Define a forward method containing the forward pass computation
         
-        return self.layer_2(self.layer_1(x))
+        x = torch.sigmoid(self.layer_1(x))
+        x = self.layer_2(x)
+        
+        return x
+    
+# Calculate accuracy (a classification metric)
+
+def accuracy_fn(y_true, y_pred):
+    
+    correct = torch.eq(y_true, y_pred).sum().item() # torch.eq() calculates where two tensors are equal
+    acc = (correct / len(y_pred)) * 100 
+    return acc    
